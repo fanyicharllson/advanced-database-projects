2363ff5dfc8a96a2dafc2a1947f0675a4106db9e
melita2024 <kelongmelita80@gmail.com>
2026-03-18 11:27:47 +0100

    feat: enhance ProfilePage styling with Tailwind CSS classes

--- web-app/app/(pages)/profile/page.tsx
+++ web-app/app/(pages)/profile/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ProfilePage() {
   return (
-    <div>ProfilePage Screen </div>
+    <div className='text-purple-500 font-bold italic flex items-center justify-center min-h-100'>ProfilePage  </div>
   )
 }
 
